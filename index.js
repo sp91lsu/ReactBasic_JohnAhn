91d3dbbcfcf18ae2aa5375885e6213f0227ebc08
dev웅 <dev.lsw91@gmail.com>
2022-12-26 15:14:10 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
 
 
 const mongoose = require('mongoose')
+
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://sw-lee:8479@cluster0.7axgnk7.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true
 }).then(()=> console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!~~안녕하세요'))
+app.get('/', (req, res) => res.send('Hello World!~~새해복많이받으세요'))
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success : false, err})
+        return res.status(200).json({
+            success : true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-0
